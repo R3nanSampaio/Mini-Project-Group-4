508b5c2387934e4453385d338c865790bf499c9b
Gabriel <gabo7_morales@hotmail.com>
2025-03-20 20:00:55 -0700

    Create About Us component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TestComponent } from './test/test.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
     {path:"home", component: HomeComponent},
     {path:'', component: TestComponent},
-    {path:'test', component: TestComponent}
-
+    {path:'test', component: TestComponent},
+    {path:'about-us', component: AboutUsComponent}
 
 ];
