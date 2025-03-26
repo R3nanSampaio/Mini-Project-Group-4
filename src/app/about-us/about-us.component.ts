598b24e7a646e47a2d533ef71336c897aaa2c127
Gabriel <gabo7_morales@hotmail.com>
2025-03-25 18:06:58 -0700

    About us info

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent {
-
 }
